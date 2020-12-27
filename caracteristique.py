a234a6bb1503c2382f49cc2db40a1d1af9bbd4e4
JOSSELIN SOMERVILLE ROBERTS <josselin.somerville@gmail.com>
2020-12-28 00:18:32 +0100

    [ADD] Add of Objet, Personnage, Cracteristique, CaracChiffree and Type

---
+++ caracteristique.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 23:53:41 2020
+
+@author: josse
+"""
+
+from psclib.diversifieur import diversifier
+
+
+class Type:
+
+  def __init__(self,type):
+    self.type = type
+
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return self.type == other.type
+    return False
+
+
+class Caracteristique:
+
+  def getAvailable():
+    return ["beaute", "couleur", "taille", "tailleH", "metier", "ageH", "mysterieux", "bavard", "curiosite", "colere"]
+
+  def getAlike(s):
+    """ Renvoie toutes les caractéristiques qui contiennent s dans leur nom"""
+    liste = []
+    for carac in Caracteristique.getAvailable():
+      if s in carac:
+        liste.append(carac)
+    return liste
+
+  def __init__(self, lib=None, types=None, intervals=None, keepLib=True, name=None):
+    if not(name is None):
+      if not(name in Caracteristique.getAvailable()) : raise NameError('Il n\'y a pas de Caracetiristique nommée ' + name)
+      if name == "beaute": Caracteristique.__init__(self, "beau", [liste_types["physique"]]),
+      if name == "couleur": Caracteristique.__init__(self, "couleur", [liste_types["physique"]], [[0,"rouge"], [2,"bleue"]], False),
+      if name == "taille" : Caracteristique.__init__(self, "taille",[liste_types["physique"]], [[0,"minuscule"],[2,"petit"],[4,"moyen"],[6,"grand"],[8,"très grand"],[10,"gigantesque"]],False),
+      if name == "tailleH" : Caracteristique.__init__(self, "grand",[liste_types["physique"]]),
+      if name == "metier" : Caracteristique.__init__(self, "métier",[liste_types["physique"]], [[0,"boulanger"],[1,"patissier"],[2,"cordonnier"],[3,"barman"],[3,"marchand"],[4,"garde"],[5,"fonctionnaire"]] ) ,                                   
+      if name == "ageH" : Caracteristique.__init__(self, "age",[liste_types["physique"]],[[0, "nourrisson"],[2,"petit"],[4,"enfant"],[6,"adolescent"],[8, "jeune adulte"],[10,"adulte"],[12, "mur"],[14,"vieux"],[16,"très vieux"],[18,"croûlant"]]),
+      if name == "mysterieux" : Caracteristique.__init__(self, "mystérieux",[liste_types["caractere"]])
+      if name == "bavard" : Caracteristique.__init__(self, "bavard",[liste_types["caractere"]])
+      if name == "curiosite" : Caracteristique.__init__(self, "curieux",[liste_types["caractere"]])
+      if name == "colere" : Caracteristique.__init__(self, "en colère",[liste_types["caractere"]])
+    else:
+      self.lib = lib # adjectif de la caracterisitque (par exemple beau) : str
+      self.keepLib = keepLib # Est ce qu'on affiche le libellé apres le facteur d echelle : bool
+      self.types = types # liste de types (par exemple physique, intelect) : liste de Type
+      if types is None:
+        self.types = []
+      self.intervals = intervals
+      if intervals is None:
+        self.intervals = [[0, "vraiment pas"], [1, "pas"], [3, "pas très"], [5, ""], [7, "plutot"], [9, "très"], [10, "super"]]
+
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return self.lib == other.lib
+    return False
+
+  def toText(self, value):
+    prep = ""
+    i = len(self.intervals) - 1
+    continuer = True
+
+    while continuer and i >= 0:
+      if value >= self.intervals[i][0]:
+        prep = self.intervals[i][1]
+        continuer = False
+      i -= 1
+
+    return diversifier(prep + " " + self.lib*self.keepLib)
+
+
+class CaracChiffree:
+
+  def __init__(self, carac=None, value=None, name=None):
+    if not(name is None):
+      if name in Caracteristique.getAvailable():
+        self = CaracChiffree.__init__(self, carac=Caracteristique(name=name), value=value)
+      else:
+        raise NameError('Il n\'y a pas de Caracteristique nommée ' + name)
+    else:
+      self.carac = carac
+      self.value = value
+
+
+  def isSame(self, other):
+    """ Renvoie si \"other\" est égal à self.carac (pas de comparaison de valeur)"""
+    if isinstance(other, CaracChiffree) : return self.carac == other.carac
+    if isinstance(other, Caracteristique) : return self.carac == other
+    raise RuntimeError("Impossible de comparer une CaracChiffrée avec ", type(other))
+
+  def toText(self):
+    return self.carac.toText(self.value)
+
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return ((self.carac == other.carac) and (self.value == other.value))
+    return False
+  
+  def __gt__(self, other):
+    if isinstance(other, self.__class__):
+      return ((self.carac == other.carac) and (self.value > other.value))
+    return False
+
+
+liste_types = {"physique": Type("physique"),
+               "psychologique": Type("psychologique"),
+               "caractere": Type("caractere")}
---
+++ objet.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 23:50:39 2020
+
+@author: josse
+"""
+
+from psclib.diversifieur import correct, diversifier
+from psclib.caracteristique import CaracChiffree
+import random
+
+class Objet:
+
+  def __init__(self, dico=None):
+    self.id = None
+    self.lib = None
+    self.proprio = None
+    self.caracs = []
+    self.noms = None
+    self.quantite = 1
+
+    if not(dico is None):
+      if "lib" in dico:
+        self.lib = dico["lib"]
+      if "proprio" in dico:
+        self.proprio = dico["proprio"]
+      if "caracs" in dico:
+        self.caracs = dico["caracs"]
+      if "noms" in dico:
+        self.noms = dico["noms"]
+    
+  def __str__(self):
+    return str([self.id,self.proprio,self.caracs,self.noms])
+
+  def ecrire(self):
+    return self.toText()
+
+  def setCarac(self, caracChif):
+    """Assigne \"caracChif\" (une instance de CaracChiffree) à l'objet. S'occupe notamment de
+    gérer si la carac doit être ajoutée ou modifier car déja présente."""
+    alreadyExists = False
+    for c in self.caracs:
+      if c.isSame(caracChif):
+        alreadyExists = True
+        c.value = caracChif.value
+    if not(alreadyExists):
+      self.caracs.append(caracChif)
+
+
+  def getCaracValue(self, carac):
+    """Retourne la valeur de \"carac\" (une instance de Caracteristique). Renvoie -1 si la carac n'est pas présente"""
+    for c in self.caracs:
+      if c.isSame(carac):
+        return c.value
+    return -1 # On ne renvoie pas None car None correspond au fait qu'on ne connaisse pas la valeur, ici ça veut dire qu'elle n'existe pas (c'est différent)
+
+
+  def toText(self, types=None, locuteur=None, interlocuteur=None, mentionProprio=True):
+    """
+    Si la liste des \"noms\" est fournie, choisi au hasard pami ces noms
+    Sinon crée un nom à partir du libellé, des caracs et du proprio
+    - types : liste de couples [proba(float), type(str)]
+    """
+
+    if locuteur is None : locuteur = Personnage()
+    if interlocuteur is None : interlocuteur = Personnage() 
+    personne = 3
+    if locuteur == self.proprio:
+      personne = 1
+    elif interlocuteur == self.proprio:
+      personne = 2
+    if self.quantite > 1:
+      personne += 3
+
+    listePossessifs = ["mon", "ton", "son", "mes", "tes", "leurs"]
+    usePossessif = False
+    if personne % 3 != 0:
+      usePossessif = True
+
+    if not(self.noms is None) and len(self.noms) > 0:
+      return random.choice(self.noms)
+
+
+    # Nom de l'objet
+    exp = "lae " + self.lib # J AI ENLEVE LA DIVERSIFICATION
+    #print("exp", exp)
+   
+    # On cherche les caracteristiques que l'on va preciser
+    caracs_a_preciser = []
+    if not(types is None):
+      for t in types:
+        for c in self.caracs:
+          if t[1] in c.types:
+            # On randomise
+            if random.random() <= t[0]:
+              caracs_a_preciser.append(c)
+
+    # On ajoute les caracteristiques
+    for c in caracs_a_preciser:
+      exp += " " + c.toText() + " et"
+    if len(caracs_a_preciser) > 0:
+      exp = exp[:-3]
+
+
+
+    # Add proprio explicite
+    if self.proprio is None: # On considère que c'est indéfini
+      exp = "un " + exp[4:]
+    elif usePossessif:
+      exp = listePossessifs[personne-1] + " " +exp[4:]
+    elif mentionProprio:
+      exp += " de " + self.proprio.prenom + " " + self.proprio.nom
+    
+    exp = correct(exp)
+    return exp
+
+
+
+class Personnage(Objet):
+
+  def getAvailable():
+    return ["inconnu", "lancelot", "mickey", "joe", "marcel", "jackie"]
+
+  def getAlike(s):
+    """ Renvoie tous les Personnage qui contiennent s dans leur nom"""
+    liste = []
+    for perso in Personnage.getAvailable():
+      if s in perso:
+        liste.append(perso)
+    return liste
+  
+  def __init__(self, dico=None, name=None):
+
+    if not(name is None):
+      if not(name in Personnage.getAvailable()) : raise NameError('Il n\'y a pas de Personnage nommé ' + name)
+      if name == "inconnu": self = Personnage.__init__(self, dico={"nom":"Inconnu", "prenom":"Soldat"})
+      if name == "lancelot": self = Personnage.__init__(self, dico={"nom":"Du Lac", "prenom":"Lancelot"})
+      if name == "mickey": self = Personnage.__init__(self, dico={"nom":"Mouse", "prenom":"Mickey"})
+      if name == "joe": self = Personnage.__init__(self, dico={"nom":"Dalton", "prenom":"Joe"})
+      if name == "marcel": self = Personnage.__init__(self, dico={"nom":"", "prenom":"Marcel", "caracs": [CaracChiffree(name="bavard", value=3)]})
+      if name == "jackie": self = Personnage.__init__(self, dico={"nom":"", "prenom":"Jackie", "caracs": [CaracChiffree(name="curiosite", value=10)]})
+    else:
+      Objet.__init__(self, dico=dico) # A RAJOUTER dico EN ARGUMENT
+      self.setCarac(CaracChiffree(name="mysterieux", value=5))
+      self.setCarac(CaracChiffree(name="bavard", value=5))
+      self.setCarac(CaracChiffree(name="curiosite", value=5))
+      self.nom = None
+      self.prenom = None
+      self.id = None
+      self.sexe = None
+      self.age = None
+
+      if not(dico is None):
+        #juste pour comparer facilement
+        if "nom" in dico:
+          self.nom = dico["nom"] #Nom est un string
+        if "prenom" in dico:
+          self.prenom = dico["prenom"] #Prenom est un string
+        if "age" in dico:
+          self.age = dico["age"] #Age est un int
+        if "sexe" in dico:
+          self.sexe = dico["sexe"] #Sexe est -non défini- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      #rajouter humeur, famille, métier, etc (pas que des valeurs fixes)
+   
+      if not (self.prenom is None) and not (self.nom is None):
+        self.id = self.prenom+self.nom #Id est un string (plus simple pour comparer des personnages)
+
+      self.contacts = [] #Liste d'objets Personnage, représente les gens connus par le personnage (ainsi que, plus tard, les informations connues par le personnage sur ses contacts)
+      self.histoires = [] #Liste d'objets Histoire, représente les histoires connues par le personnage, peu importe qu'il soit le narrateur ou non
+
+  
+  def copyStrip(self): #Retourne les caractéristiques de base sous forme d'un dictionnaire
+    return Personnage({"nom":self.nom, "prenom":self.prenom, "age":self.age, "sexe":self.sexe})
+  
+
+  def imprimer(self, texte) : #Méthode pour faire parler le personnage (on ajoute juste qui parle avant le texte)
+    return self.prenom + " " + self.nom + " : " + diversifier(texte)
+    
+  
+  def getContact(self, id) : #Retourne la vision qu'a "self" du personnage "id" - A COMPLETER LORSQU'ON TRAITERA LES ECHANGES D'INFORMATIONS
+    for p in self.contacts :
+      if p.id == id :
+        return p
+    return None
+
+
+  def toText(self, locuteur=None, interlocuteur=None):
+    return self.prenom + " " + self.nom
