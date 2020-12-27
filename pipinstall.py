6b68ab912f6f916b797c9714ea745b01d97272d4
JOSSELIN SOMERVILLE ROBERTS <josselin.somerville@gmail.com>
2020-12-28 00:39:01 +0100

    [CHANGE] importAll() now does nothing and only shows what to import through help(importAll)

--- pipinstall.py
+++ pipinstall.py
@@ -15,7 +15,17 @@ def install(package):
 def installRequirements():
     install('verbecc')
     install('google_trans_new')
+    install('language_tool_python')
     
 def importAll():
-    from psclib.diversifieur import Diversifieur
+    """
+    from psclib.diversifieur import correct, cong, diversifier, get_syn
     from psclib.action import Action
+    from psclib.objet import Objet, Personnage
+    from psclib.caracteristique import Caracteristique, Type, CaracChiffree
+    from psclib.histoire import Histoire, ajouterPonctuation, demanderLien, nePasSavoirLien
+    from psclib.coeuraction import CoeurAction
+    from psclib.coeurdescriptif import CoeurDescriptif
+    from psclib.lien import Lien
+    """
+    pass
