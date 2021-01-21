38f7405d943dac1391ba324f115714ef401e6664
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2021-01-21 19:47:05 +0100

    [MODIF] Passage des graphiques en HTML 7

--- coeuraction.py
+++ coeuraction.py
@@ -23,13 +23,15 @@ class CoeurAction(Coeur) :
       
 
   def getGraphText(self):
-      s = "<B>COEUR ACTION</B>" + "\n"
+      s = """<table border="1" cellborder="0" cellspacing="1">\n"""
+      s+= """  <tr><td align="center"><b>COEUR ACTION</b></td></tr>\n"""
       if type(self.sujet) == list:
-          s+= "<I>Sujet:</I> " + " et ".join([s.getGraphText() for s in self.sujet]) + "\l"
+          s+= """  <tr><td align="left">Sujet:</I> """ + """ et """.join([s.getGraphText() for s in self.sujet]) + """</td></tr>\n"""
       else:
-          s+= "<I>Sujet:</I> " + self.sujet.getGraphText() + "\l"
-      s+= "<I>Action:</I> " + self.action.getGraphText() + "\l"
-      if not(self.cod is None): s+= "<I>Complément:</I> " + self.cod.getGraphText() + "\l"
+          s+= """  <tr><td align="left">Sujet:</I> """ + self.sujet.getGraphText() + """</td></tr>\n"""
+      s+= """  <tr><td align="left">Action:</I> """ + self.action.getGraphText() + """</td></tr>\n"""
+      if not(self.cod is None): s+= """  <tr><td align="left">Complément:</I> """ + self.cod.getGraphText() + """</td></tr>\n"""
+      s+="""</table>"""
       return s
 
     
